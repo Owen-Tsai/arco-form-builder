bac142795f9420f25c30a618f9793393e930775e
Owen Tsai <owentsai.v@gmail.com>
2023-02-06 09:54:15 +0800

    feat: radio & checkbox

---
+++ src/components/renderer/widgets/Checkbox.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-radio-group
+    v-model="form[uid]"
+    :direction="config.direction"
+    :disabled="config.disabled"
+  >
+    <a-radio v-for="(opt, i) in options" :key="i" :value="opt.value">
+      {{ opt.label }}
+    </a-radio>
+  </a-radio-group>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue'
+import { OptRadio } from '@/types/widget'
+import useForm from '@/hooks/use-form-injection'
+import { useDataSource } from '@/hooks/use-data-source'
+
+const props = defineProps({
+  config: {
+    type: Object as PropType<OptRadio>,
+    required: true,
+  },
+  uid: {
+    type: String,
+    required: true,
+  },
+})
+
+const options = useDataSource(
+  props.config.dataSourceType,
+  props.config.dataSource
+)
+
+const { form } = useForm()
+</script>
